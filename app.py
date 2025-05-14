b678e8f362a55e8e59c1b73ce7df7f8611097d2e
stepan <stepanaulchenko@gmail.com>
2025-05-14 11:53:10 +0300

    Initial commit

--- app.py
+++ app.py
@@ -40,21 +40,41 @@ def handle_dialog(res, req):
         res['response']['text'] = 'Ты не указал ни одного города!'
     elif len(cities) == 1:
         country = get_geo_info(cities[0], 'country')  # Используем новую функцию
-        res['response']['text'] = f'{cities[0]} находится в стране — {country}'
+        res['response']['text'] = f'{cities[0].capitalize()} находится в стране — {country}'
     elif len(cities) == 2:
         coords_1 = get_geo_info(cities[0], 'coordinates')  # Получение координат первого города
         coords_2 = get_geo_info(cities[1], 'coordinates')  # Получение координат второго города
         distance = get_distance(coords_1, coords_2)
-        res['response']['text'] = f'Расстояние между {cities[0]} и {cities[1]} составляет примерно {round(distance)} километров.'
+        res['response']['text'] = f'Расстояние между {cities[0].capitalize()} и {cities[1].capitalize()} составляет примерно {round(distance)} километров.'
     else:
         res['response']['text'] = 'Укажи меньше городов, пожалуйста!'
 
 
+# Добавляем расширенное логирование
 def get_cities(req):
     cities = []
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.GEO' and 'city' in entity['value'].keys():
-            cities.append(entity['value']['city'])
+    entities = req.get('request', {}).get('nlu', {}).get('entities', [])
+    logging.info(f'Entities received: {json.dumps(entities, indent=2)}')
+
+    for entity in entities:
+        if entity.get('type') == 'YANDEX.GEO':
+            value = entity.get('value', {})
+            logging.info(f'Processing YANDEX.GEO entity with value: {json.dumps(value, indent=2)}')
+
+            # Проверяем наличие полей с городом различными способами
+            city_name = None
+            possible_keys = ['city', 'locality', 'geo_object']
+            for key in possible_keys:
+                if key in value:
+                    city_name = value[key]
+                    break
+
+            if city_name:
+                cities.append(city_name.strip())
+                logging.info(f'Added city: {city_name}')
+            else:
+                logging.warning(f'Skipped entity without valid city name: {entity}')
+
     return cities
 
 if __name__ == '__main__':
